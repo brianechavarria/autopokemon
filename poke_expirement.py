51c59c0487981f0eac49ca8b17a5e6b543dcc1f7
brianechavarria <brianechavarria7@gmail.com>
2026-04-03 19:36:31 -0500

    planning an experiment tool

---
+++ poke_expirement.py
@@ -0,0 +1,18 @@
+#The purpose of this file is to run experiments. Expected values are a reasonable tool for prediction and keep cost low but adding simulation to this would just be fun
+import copy
+from typing import Tuple
+from pokeAI import Battle
+from battle_ai import *
+
+#need the functions from battle_ai to make this work
+
+
+#TODO add function to generate a team
+
+
+#TODO add a function to import team from the DesEMU session
+
+
+
+#TODO run the simulation n number of times and calculate how often you win given you make the right move. 
+#Use this for experimentation but also as a way to judge each move that's made and grade the player as well as show live win probability
